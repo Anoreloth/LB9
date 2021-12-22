cf89bfd6a8207b1b396b580e8f91325ab1598e9e
Anoreloth <93758322+Anoreloth@users.noreply.github.com>
2021-12-22 13:34:20 +0200

    Create Program.cs

---
+++ LB9_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LB9_2
+{
+    class Trapezium
+    {
+        public double Calc(double a, double b, int n)
+        {
+            double h, x1, x2, res, sum = 0;
+            h = (b - a) / n;
+            for (int i = 0; i < n; i++)
+            {
+                x1 = a + i * h;
+                x2 = a + (i + 1) * h;
+                sum += (Math.Exp(Math.Sin(x1)) + Math.Exp(Math.Sin(x2)));
+            }
+            res = sum * h / 2;
+            return Math.Round(res, 3);
+        }
+    }
+    public delegate double Fun(double a, double b, int n);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trapezium t = new Trapezium();
+            Fun x = new Fun(t.Calc);
+            Console.WriteLine($"Вiдповiдь: {x(2, 5, 3)}");
+        }
+    }
+}
